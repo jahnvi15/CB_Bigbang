d7953811f493808e14c7ffa8b53a847058755a9d
jahnvi15 <91893464+jahnvi15@users.noreply.github.com>
2023-08-19 16:17:51 +0530

    Upadted index.js

--- js/index.js
+++ js/index.js
@@ -12,44 +12,43 @@ document.getElementById("contact-form").addEventListener("submit", async functio
 
     if (response.ok) {
       console.log("Form data submitted successfully.");
-      // You can perform additional actions here if needed.
+      
       const nameInput = formData.get('name');
       const enrollInput = formData.get('enroll');
-
-      // Draw the ticket here using the nameInput and enrollInput values
+      
       const canvas = document.getElementById("canvas");
       const ctx = canvas.getContext("2d");
+      canvas.width = 1869;
+canvas.height = 884;
       var background = new Image();
-background.crossOrigin = "Anonymous";
-background.src =
-  "https://res.cloudinary.com/pooja-gera/image/upload/v1650903571/OrientationTicket_v2_0.5perc_thtcyl.png";
+      background.crossOrigin = "Anonymous";
+      background.src = "https://res.cloudinary.com/pooja-gera/image/upload/v1650903571/OrientationTicket_v2_0.5perc_thtcyl.png";
 
-      ctx.drawImage(background, 0, 0);
-      ctx.fillStyle = "#ffffff";
-      ctx.font = "50px Poppins, sans-serif";
-      // change position or placement of name here
-      ctx.fillText(nameInput, 1290, 360);
-      ctx.fillStyle = "#ffffff";
-      ctx.font = "40px Poppins, sans-serif";
-      // change position or placement of ticketNumber here
-      ctx.fillText(enrollInput, 325, 805);
+      background.onload = function () {
+        ctx.drawImage(background, 0, 0);
+        ctx.fillStyle = "#ffffff";
+        ctx.font = "50px Poppins, sans-serif";
+        ctx.fillText(nameInput, 1290, 360);
+        ctx.fillStyle = "#ffffff";
+        ctx.font = "40px Poppins, sans-serif";
+        ctx.fillText(enrollInput, 325, 805);
 
-      document.getElementById("canvas").style.display = "block";
-      document.getElementById("download").style.display = "block";
-      document.getElementById("head").style.display = "block";
-      document.getElementById("hide").style.display = "none";
+        document.getElementById("canvas").style.display = "block";
+        document.getElementById("download").style.display = "block";
+        document.getElementById("head").style.display = "block";
+        document.getElementById("hide").style.display = "none";
+      };
 
       const download = document.getElementById("download");
-
-download.addEventListener("click", function () {
-  const mycanvas = document.getElementById("canvas");
-  var url = mycanvas.toDataURL();
-  let a = document.createElement("a");
-  a.setAttribute("href", url);
-  a.setAttribute("download", `Bigbang_${enrollInput.value}_${nameInput.value}`);
-  a.click();
-  a.remove();
-});
+      download.addEventListener("click", function () {
+        const mycanvas = document.getElementById("canvas");
+        var url = mycanvas.toDataURL();
+        let a = document.createElement("a");
+        a.setAttribute("href", url);
+        a.setAttribute("download", `Bigbang_${enrollInput}_${nameInput}.png`);
+        a.click();
+        a.remove();
+      });
     } else {
       console.error("Form submission failed.");
       // Handle the error or provide user feedback.
@@ -59,66 +58,122 @@ download.addEventListener("click", function () {
     // Handle the error or provide user feedback.
   }
 });
-var canvas = document.getElementById("canvas"),
-  ctx = canvas.getContext("2d");
 
-canvas.width = 1869;
-canvas.height = 884;
+// document.getElementById("contact-form").addEventListener("submit", async function (e) {
+//   e.preventDefault(); // Prevent the default form submission behavior
 
-var background = new Image();
-background.crossOrigin = "Anonymous";
-background.src =
-  "https://res.cloudinary.com/pooja-gera/image/upload/v1650903571/OrientationTicket_v2_0.5perc_thtcyl.png";
+//   const formData = new FormData(this);
+//   const url = "https://api.sheetmonkey.io/form/dymgKGcYxGbLqZhFpMhBdy";
 
-// const nameInput = document.getElementById("name");
-(function(){
-        const queryString = window.location.search;
-        const urlParams = new URLSearchParams(queryString);
-        const nameInput = urlParams.get('name')
-        document.getElementById('name').value = nameInput;
-      })();
-const enrollInput = document.getElementById("enroll");
-const submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  ctx.drawImage(background, 0, 0);
-  ctx.fillStyle = "#ffffff";
-  ctx.font = "50px Poppins, sans-serif";
-  // change position or placement of name here
-  ctx.fillText(nameInput.value, 1290, 360);
-  ctx.fillStyle = "#ffffff";
-  ctx.font = "40px Poppins, sans-serif";
-  // change position or placement of ticketNumber here
-  ctx.fillText(enrollInput.value, 325, 805);
-  document.getElementById("canvas").style.display = "block";
-  document.getElementById("download").style.display = "block";
-  document.getElementById("head").style.display = "block";
-  document.getElementById("hide").style.display = "none";
-});
+//   try {
+//     const response = await fetch(url, {
+//       method: "POST",
+//       body: formData,
+//     });
 
-var name = "";
-var ticketNumber = "";
+//     if (response.ok) {
+//       console.log("Form data submitted successfully.");
+//       // You can perform additional actions here if needed.
+//       const nameInput = formData.get('name');
+//       const enrollInput = formData.get('enroll');
+      
+//       // Draw the ticket here using the nameInput and enrollInput values
+//       const canvas = document.getElementById("canvas");
+//       const ctx = canvas.getContext("2d");
+//       var background = new Image();
+// background.crossOrigin = "Anonymous";
+// background.src =
+//   "https://res.cloudinary.com/pooja-gera/image/upload/v1650903571/OrientationTicket_v2_0.5perc_thtcyl.png";
 
-background.onload = function () {
-  ctx.drawImage(background, 0, 0);
-  ctx.fillStyle = "#ffffff";
-  ctx.font = "50px Poppins, sans-serif";
-  // change position or placement of name here
-  ctx.fillText(name, 1290, 360);
-  ctx.fillStyle = "#ffffff";
-  ctx.font = "40px Poppins, sans-serif";
-  // change position or placement of ticketNumber here
-  ctx.fillText(ticketNumber, 325, 805);
-};
+//       ctx.drawImage(background, 0, 0);
+//       ctx.fillStyle = "#ffffff";
+//       ctx.font = "50px Poppins, sans-serif";
+//       // change position or placement of name here
+//       ctx.fillText(nameInput, 1290, 360);
+//       ctx.fillStyle = "#ffffff";
+//       ctx.font = "40px Poppins, sans-serif";
+//       // change position or placement of ticketNumber here
+//       ctx.fillText(enrollInput, 325, 805);
 
-const download = document.getElementById("download");
+//       document.getElementById("canvas").style.display = "block";
+//       document.getElementById("download").style.display = "block";
+//       document.getElementById("head").style.display = "block";
+//       document.getElementById("hide").style.display = "none";
 
-download.addEventListener("click", function () {
-  const mycanvas = document.getElementById("canvas");
-  var url = mycanvas.toDataURL();
-  let a = document.createElement("a");
-  a.setAttribute("href", url);
-  a.setAttribute("download", `Bigbang_${enrollInput.value}_${nameInput.value}`);
-  a.click();
-  a.remove();
-});
+//       const download = document.getElementById("download");
+
+// download.addEventListener("click", function () {
+//   const mycanvas = document.getElementById("canvas");
+//   var url = mycanvas.toDataURL();
+//   let a = document.createElement("a");
+//   a.setAttribute("href", url);
+//   a.setAttribute("download", `Bigbang_${enrollInput.value}_${nameInput.value}`);
+//   a.click();
+//   a.remove();
+// });
+//     } else {
+//       console.error("Form submission failed.");
+//       // Handle the error or provide user feedback.
+//     }
+//   } catch (error) {
+//     console.error("An error occurred:", error);
+//     // Handle the error or provide user feedback.
+//   }
+// });
+// var canvas = document.getElementById("canvas"),
+//   ctx = canvas.getContext("2d");
+
+// canvas.width = 1869;
+// canvas.height = 884;
+
+// var background = new Image();
+// background.crossOrigin = "Anonymous";
+// background.src =
+//   "https://res.cloudinary.com/pooja-gera/image/upload/v1650903571/OrientationTicket_v2_0.5perc_thtcyl.png";
+
+// // const nameInput = document.getElementById("name");
+// const enrollInput = document.getElementById("enroll");
+// const submitBtn = document.getElementById("submitBtn");
+// submitBtn.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   ctx.drawImage(background, 0, 0);
+//   ctx.fillStyle = "#ffffff";
+//   ctx.font = "50px Poppins, sans-serif";
+//   // change position or placement of name here
+//   ctx.fillText(nameInput.value, 1290, 360);
+//   ctx.fillStyle = "#ffffff";
+//   ctx.font = "40px Poppins, sans-serif";
+//   // change position or placement of ticketNumber here
+//   ctx.fillText(enrollInput.value, 325, 805);
+//   document.getElementById("canvas").style.display = "block";
+//   document.getElementById("download").style.display = "block";
+//   document.getElementById("head").style.display = "block";
+//   document.getElementById("hide").style.display = "none";
+// });
+
+// var name = "";
+// var ticketNumber = "";
+
+// background.onload = function () {
+//   ctx.drawImage(background, 0, 0);
+//   ctx.fillStyle = "#ffffff";
+//   ctx.font = "50px Poppins, sans-serif";
+//   // change position or placement of name here
+//   ctx.fillText(name, 1290, 360);
+//   ctx.fillStyle = "#ffffff";
+//   ctx.font = "40px Poppins, sans-serif";
+//   // change position or placement of ticketNumber here
+//   ctx.fillText(ticketNumber, 325, 805);
+// };
+
+// const download = document.getElementById("download");
+
+// download.addEventListener("click", function () {
+//   const mycanvas = document.getElementById("canvas");
+//   var url = mycanvas.toDataURL();
+//   let a = document.createElement("a");
+//   a.setAttribute("href", url);
+//   a.setAttribute("download", `Bigbang_${enrollInput.value}_${nameInput.value}`);
+//   a.click();
+//   a.remove();
+// });
